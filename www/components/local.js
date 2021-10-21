015e759fe262d69e5929001f9c616818a2693d7e
Demershow <demersontorres520@gmail.com>
2021-10-22 06:45:12 +0900

    Project's Initial Commit

---
+++ www/components/local.js
@@ -0,0 +1,29 @@
+var valor = 0;
+
+$(document).on("click", "#salvar", function(){
+  localStorage.setItem(valor, $("#nome").val());
+  valor++;
+});
+
+$(document).on("click", "#buscar", function(){
+  var pessoa = localStorage.getItem($("#identificador").val());
+  $("#nomeBusca").val(pessoa);
+});
+
+
+$(document).on("click", "#removRegistro", function(){
+  localStorage.clear();
+  limparcampos();
+});
+
+
+$(document).on("click", "#remover", function(){
+  localStorage.removeItem($("#identificador").val());
+  limparcampos();
+});
+
+function limparcampos(){
+  $("#nome").val("");
+  $("#nomeBusca").val("");
+  $("#identificador").val("");
+}
